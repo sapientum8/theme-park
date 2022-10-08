c3428b9061a5a1bf3446611d14187c9f860d82a5
sapientum8 <sapientum08@gmail.com>
2022-10-08 19:01:07 -0400

    initial commit

---
+++ api/src/main/java/com/gradlehero/themepark/ThemeParkRide.java
@@ -0,0 +1,5 @@
+package com.gradlehero.themepark;
+
+public record ThemeParkRide(String name,String description,String status,String imagePath) {
+
+}
---
+++ service/src/main/java/com/gradlehero/themepark/Doctor.java
@@ -0,0 +1,9 @@
+package com.gradlehero.themepark;
+
+import lombok.Data;
+
+@Data
+public class Doctor {
+    private int id;
+    private String name;
+}

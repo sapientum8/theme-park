b7584f6163b8ed8777880175b518fc18af7c64f8
sapientum8 <sapientum08@gmail.com>
2022-10-08 21:38:11 -0400

    refactoring changes

--- service/src/main/java/com/gradlehero/themepark/Doctor.java
+++ service/src/main/java/com/gradlehero/themepark/Doctor.java
@@ -1,8 +1,10 @@
 package com.gradlehero.themepark;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Doctor {
     private int id;
     private String name;
